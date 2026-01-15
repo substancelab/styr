c883083034bbccf2f290e1000ca90bcba931b2a5
Jakob Skjerning <jakob@mentalized.net>
2026-01-15 10:35:35 +0100

    Remove superfluous class method 'from_config'

--- lib/styr/backend/ssh_backend.rb
+++ lib/styr/backend/ssh_backend.rb
@@ -27,21 +27,5 @@ class Styr
         "SSH"
       end
     end
-
-    class << self
-      def from_config(config)
-        backend_type = config.fetch("backend")
-        backend_class =
-          case backend_type.downcase
-          when "heroku"
-            HerokuBackend
-          when "ssh"
-            SSHBackend
-          else
-            raise "Unknown backend type: #{backend_type}"
-          end
-        backend_class.new(config)
-      end
-    end
   end
 end
